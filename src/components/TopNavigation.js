b5b8dbed4933badbe8e78ae67e310aa9fefef1ff
Othman Mohammed <uniqueginun@gmail.com>
2020-03-22 08:29:46 +0300

    add some mobile responsiveness

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -19,6 +19,18 @@ export default function TopNavigation() {
     <nav className="navbar navbar-inverse">
       <div className="container-fluid">
         <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
           <a className="navbar-brand" href="/">
             <img
               alt="Brand"
